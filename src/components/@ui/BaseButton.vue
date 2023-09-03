5df9b8231c2744b6cf8ed6dc2b9c41475c931cff
mkstas <mstasmakarov@yandex.ru>
2023-09-03 20:03:55 +0300

    dev: optimize creating BaseButton

--- src/components/@ui/BaseButton.vue
+++ src/components/@ui/BaseButton.vue
@@ -1,5 +1,13 @@
-<script setup>
+<script>
 import { BUTTON_TYPE_NEUTRAL, BUTTON_TYPE_DANGER } from '@/constans';
+
+const typeClasses = {
+  [BUTTON_TYPE_NEUTRAL]: 'bg-gray-100 enabled:hover:bg-gray-200',
+  [BUTTON_TYPE_DANGER]: 'bg-red-500 enabled:hover:bg-red-600 text-white',
+};
+</script>
+
+<script setup>
 import { isButtonTypeValid } from '@/validators';
 
 defineProps({
@@ -9,11 +17,6 @@ defineProps({
     validator: isButtonTypeValid,
   },
 });
-
-const typeClasses = {
-  [BUTTON_TYPE_NEUTRAL]: 'bg-gray-100 enabled:hover:bg-gray-200',
-  [BUTTON_TYPE_DANGER]: 'bg-red-500 enabled:hover:bg-red-600 text-white',
-};
 </script>
 
 <template>
