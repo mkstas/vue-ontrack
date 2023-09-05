ed35f101a13ad90144b4adbc3b75a0b75d02a04c
mkstas <mstasmakarov@yandex.ru>
2023-09-05 17:38:47 +0300

    dev: create timer logick

--- src/components/timeline/TimelineStopwatch.vue
+++ src/components/timeline/TimelineStopwatch.vue
@@ -1,28 +1,51 @@
 <script setup>
+import { ref } from 'vue';
 import { ArrowPathIcon, PauseIcon, PlayIcon } from '@heroicons/vue/24/outline';
 
 import {
   BUTTON_TYPE_SUCCESS,
   BUTTON_TYPE_WARNING,
   BUTTON_TYPE_DANGER,
+  MILLISECONDS_IN_SECOND,
 } from '@/constans';
 import { isNumber } from '@/validators';
 import { formatSeconds } from '@/functions';
 
 import BaseButton from '../@ui/BaseButton.vue';
 
-defineProps({
+const props = defineProps({
   seconds: {
     type: Number,
     default: 0,
     validator: isNumber,
   },
 });
+
+const seconds = ref(props.seconds);
+const isRunning = ref(false);
+
+const start = () => {
+  isRunning.value = setInterval(() => {
+    seconds.value++;
+  }, MILLISECONDS_IN_SECOND);
+};
+
+const stop = () => {
+  clearInterval(isRunning.value);
+
+  isRunning.value = false;
+};
+
+const reset = () => {
+  stop();
+
+  seconds.value = props.seconds;
+};
 </script>
 
 <template>
   <div class="flex w-full gap-2">
-    <BaseButton :type="BUTTON_TYPE_DANGER">
+    <BaseButton :type="BUTTON_TYPE_DANGER" @click="reset">
       <ArrowPathIcon class="h-8" />
     </BaseButton>
     <div
@@ -30,10 +53,10 @@ defineProps({
     >
       {{ formatSeconds(seconds) }}
     </div>
-    <BaseButton :type="BUTTON_TYPE_WARNING">
+    <BaseButton v-if="isRunning" :type="BUTTON_TYPE_WARNING" @click="stop">
       <PauseIcon class="h-8" />
     </BaseButton>
-    <BaseButton :type="BUTTON_TYPE_SUCCESS">
+    <BaseButton v-else :type="BUTTON_TYPE_SUCCESS" @click="start">
       <PlayIcon class="h-8" />
     </BaseButton>
   </div>
