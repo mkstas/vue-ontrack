6f307c7d7dfb6fde78a890efb8291418631fe30c
mkstas <makarov.stn@gmail.com>
2023-09-12 19:39:50 +0300

    refactor: change nav items representation

--- src/components/nav/NavItem.vue
+++ src/components/nav/NavItem.vue
@@ -1,12 +1,27 @@
-<script>
-export default {
-  inheritAttrs: false,
-};
+<script setup>
+import { computed } from 'vue';
+
+import { isPageValid } from '@/validators';
+
+import { currentPage, navigate } from '@/router';
+
+const props = defineProps({
+  page: {
+    type: String,
+    required: true,
+    validator: isPageValid,
+  },
+});
+
+const classes = computed(() => [
+  'flex flex-col items-center p-2 text-xs capitalize',
+  { 'bg-gray-200 pointer-events-none': props.page === currentPage.value },
+]);
 </script>
 
 <template>
   <li class="flex-1">
-    <a v-bind="$attrs" class="flex flex-col items-center p-2 text-xs capitalize">
+    <a :href="`#${page}`" :class="classes" @click="navigate(page)">
       <slot></slot>
     </a>
   </li>
